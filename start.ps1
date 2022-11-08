a8e21c45f0494f9d8a21178847afa51fe55c305f
jolerasioo <90843031+jolerasioo@users.noreply.github.com>
2022-11-08 09:08:35 +0000

    Update start.ps1

--- start.ps1
+++ start.ps1
@@ -1,68 +1,94 @@
-if (-not (Test-Path Env:AZP_URL)) {
-  Write-Error "error: missing AZP_URL environment variable"
+#!/bin/bash
+set -e
+
+if [ -z "$AZP_URL" ]; then
+  echo 1>&2 "error: missing AZP_URL environment variable"
   exit 1
-}
+fi
 
-if (-not (Test-Path Env:AZP_TOKEN_FILE)) {
-  if (-not (Test-Path Env:AZP_TOKEN)) {
-    Write-Error "error: missing AZP_TOKEN environment variable"
+if [ -z "$AZP_TOKEN_FILE" ]; then
+  if [ -z "$AZP_TOKEN" ]; then
+    echo 1>&2 "error: missing AZP_TOKEN environment variable"
     exit 1
-  }
+  fi
 
-  $Env:AZP_TOKEN_FILE = "\azp\.token"
-  $Env:AZP_TOKEN | Out-File -FilePath $Env:AZP_TOKEN_FILE
+  AZP_TOKEN_FILE=/azp/.token
+  echo -n $AZP_TOKEN > "$AZP_TOKEN_FILE"
+fi
+
+unset AZP_TOKEN
+
+if [ -n "$AZP_WORK" ]; then
+  mkdir -p "$AZP_WORK"
+fi
+
+export AGENT_ALLOW_RUNASROOT="1"
+
+cleanup() {
+  if [ -e config.sh ]; then
+    print_header "Cleanup. Removing Azure Pipelines agent..."
+
+    # If the agent has some running jobs, the configuration removal process will fail.
+    # So, give it some time to finish the job.
+    while true; do
+      ./config.sh remove --unattended --auth PAT --token $(cat "$AZP_TOKEN_FILE") && break
+
+      echo "Retrying in 30 seconds..."
+      sleep 30
+    done
+  fi
 }
 
-Remove-Item Env:AZP_TOKEN
-
-if ((Test-Path Env:AZP_WORK) -and -not (Test-Path $Env:AZP_WORK)) {
-  New-Item $Env:AZP_WORK -ItemType directory | Out-Null
+print_header() {
+  lightcyan='\033[1;36m'
+  nocolor='\033[0m'
+  echo -e "${lightcyan}$1${nocolor}"
 }
-
-New-Item "\azp\agent" -ItemType directory | Out-Null
 
 # Let the agent ignore the token env variables
-$Env:VSO_AGENT_IGNORE = "AZP_TOKEN,AZP_TOKEN_FILE"
+export VSO_AGENT_IGNORE=AZP_TOKEN,AZP_TOKEN_FILE
 
-Set-Location agent
+print_header "1. Determining matching Azure Pipelines agent..."
 
-Write-Host "1. Determining matching Azure Pipelines agent..." -ForegroundColor Cyan
+AZP_AGENT_PACKAGES=$(curl -LsS \
+    -u user:$(cat "$AZP_TOKEN_FILE") \
+    -H 'Accept:application/json;' \
+    "$AZP_URL/_apis/distributedtask/packages/agent?platform=$TARGETARCH&top=1")
 
-$base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(":$(Get-Content ${Env:AZP_TOKEN_FILE})"))
-$package = Invoke-RestMethod -Headers @{Authorization=("Basic $base64AuthInfo")} "$(${Env:AZP_URL})/_apis/distributedtask/packages/agent?platform=win-x64&`$top=1"
-$packageUrl = $package[0].Value.downloadUrl
+AZP_AGENT_PACKAGE_LATEST_URL=$(echo "$AZP_AGENT_PACKAGES" | jq -r '.value[0].downloadUrl')
 
-Write-Host $packageUrl
+if [ -z "$AZP_AGENT_PACKAGE_LATEST_URL" -o "$AZP_AGENT_PACKAGE_LATEST_URL" == "null" ]; then
+  echo 1>&2 "error: could not determine a matching Azure Pipelines agent"
+  echo 1>&2 "check that account '$AZP_URL' is correct and the token is valid for that account"
+  exit 1
+fi
 
-Write-Host "2. Downloading and installing Azure Pipelines agent..." -ForegroundColor Cyan
+print_header "2. Downloading and extracting Azure Pipelines agent..."
 
-$wc = New-Object System.Net.WebClient
-$wc.DownloadFile($packageUrl, "$(Get-Location)\agent.zip")
+curl -LsS $AZP_AGENT_PACKAGE_LATEST_URL | tar -xz & wait $!
 
-Expand-Archive -Path "agent.zip" -DestinationPath "\azp\agent"
+source ./env.sh
 
-try
-{
-  Write-Host "3. Configuring Azure Pipelines agent..." -ForegroundColor Cyan
+print_header "3. Configuring Azure Pipelines agent..."
 
-  .\config.cmd --unattended `
-    --agent "$(if (Test-Path Env:AZP_AGENT_NAME) { ${Env:AZP_AGENT_NAME} } else { hostname })" `
-    --url "$(${Env:AZP_URL})" `
-    --auth PAT `
-    --token "$(Get-Content ${Env:AZP_TOKEN_FILE})" `
-    --pool "$(if (Test-Path Env:AZP_POOL) { ${Env:AZP_POOL} } else { 'Default' })" `
-    --work "$(if (Test-Path Env:AZP_WORK) { ${Env:AZP_WORK} } else { '_work' })" `
-    --replace
+./config.sh --unattended \
+  --agent "${AZP_AGENT_NAME:-$(hostname)}" \
+  --url "$AZP_URL" \
+  --auth PAT \
+  --token $(cat "$AZP_TOKEN_FILE") \
+  --pool "${AZP_POOL:-Default}" \
+  --work "${AZP_WORK:-_work}" \
+  --replace \
+  --acceptTeeEula & wait $!
 
-  Write-Host "4. Running Azure Pipelines agent..." -ForegroundColor Cyan
+print_header "4. Running Azure Pipelines agent..."
 
-  .\run.cmd
-}
-finally
-{
-  Write-Host "Cleanup. Removing Azure Pipelines agent..." -ForegroundColor Cyan
+trap 'cleanup; exit 0' EXIT
+trap 'cleanup; exit 130' INT
+trap 'cleanup; exit 143' TERM
 
-  .\config.cmd remove --unattended `
-    --auth PAT `
-    --token "$(Get-Content ${Env:AZP_TOKEN_FILE})"
-}
+chmod +x ./run-docker.sh
+
+# To be aware of TERM and INT signals call run.sh
+# Running it with the --once flag at the end will shut down the agent after the build is executed
+./run-docker.sh "$@" & wait $!
